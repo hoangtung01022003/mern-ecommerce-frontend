64f4b42ea5233614814687b68366db765e43b633
“hoangtung01022003” <hoangtung01022003@gmail.com>
2025-03-13 00:52:47 +0700

    Update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  baseURL: API_URL,
+  withCredentials: true, // Cho phép gửi cookie/token với request
 });
 
 // const axiosInstance = axios.create({
