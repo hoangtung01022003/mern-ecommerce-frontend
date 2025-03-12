2998d7a965a0958c9eb6751f1270331903876536
“hoangtung01022003” <hoangtung01022003@gmail.com>
2025-03-12 21:42:33 +0700

    Update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// Lấy URL từ biến môi trường
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "api",
-	withCredentials: true, // send cookies to the server
+    baseURL: API_URL,
+    withCredentials: true, // Cho phép gửi cookie/token với request
 });
 
 export default axiosInstance;
